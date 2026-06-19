f1f3af9cc9e1ea72611ec40ca3a0cc434274c9b1
Abhi69-maker <abhiabhilash26609@gmail.com>
2026-06-19 23:47:18 +0530

    Initiated Project Setup

---
+++ src/main/java/com/app/springdatajpa/turtorial/productcatalogcachesystem/ProductCatalogCacheSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app.springdatajpa.turtorial.productcatalogcachesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogCacheSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogCacheSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/springdatajpa/turtorial/productcatalogcachesystem/ProductCatalogCacheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springdatajpa.turtorial.productcatalogcachesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogCacheSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
